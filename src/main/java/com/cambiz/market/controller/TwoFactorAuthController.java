10731e472010dbdb8c878c194deb4c423d180d28
MohammadMusa <musanjahnemohammad@gmail.com>
2026-04-24 10:25:37 +0100

    Final fix - disable 2FA and mail, configure PostgreSQL

--- src/main/java/com/cambiz/market/controller/TwoFactorAuthController.java
+++ src/main/java/com/cambiz/market/controller/TwoFactorAuthController.java
@@ -1,17 +1,9 @@
 package com.cambiz.market.controller;
 
-import com.cambiz.market.dto.ApiResponse;
-import com.cambiz.market.model.User;
-import com.cambiz.market.service.TwoFactorAuthService;
-import com.cambiz.market.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth/2fa")
@@ -20,9 +12,6 @@ import java.util.Map;
 @CrossOrigin(origins = "*")
 @ConditionalOnProperty(name = "security.2fa.enabled", havingValue = "true", matchIfMissing = false)
 public class TwoFactorAuthController {
-    
-    private final TwoFactorAuthService twoFactorAuthService;
-    private final UserService userService;
     
     // ... rest of the code stays the same
 }
